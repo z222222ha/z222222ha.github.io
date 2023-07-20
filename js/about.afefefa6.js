24d09b51c0dace4ae2fe6d620ddf559b00377759
ZHH <zhang2ha@qq.com>
2023-07-20 15:44:10 +0800

    update page

--- js/about.afefefa6.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkmy_blog"]=self["webpackChunkmy_blog"]||[]).push([[443],{5399:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var s=function(){var n=this;n._self._c;return n._m(0)},l=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],e=u(1001),a={},r=(0,e.Z)(a,s,l,!1,null,null,null),i=r.exports}}]);
-//# sourceMappingURL=about.afefefa6.js.map
